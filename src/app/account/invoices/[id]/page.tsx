89004a2d0367c799e25dcf43293101411d99cc03
pradeeptomer4u <pradeeptomer4u@gmail.com>
2025-08-30 10:32:21 +0530

    changes

--- src/app/account/invoices/[id]/page.tsx
+++ src/app/account/invoices/[id]/page.tsx
@@ -1,6 +1,9 @@
-import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import InvoiceDetailClient from "./InvoiceDetailClient";
 
-export default function Page({ params }: { params: { id: string } }) {
-  return <InvoiceDetailClient id={params.id} />;
+type Props = {
+  params: { id: string };
+}
+
+export default function Page(props: Props) {
+  return <InvoiceDetailClient id={props.params.id} />;
 }

32c12a53c74429fb1aebf7ad0d6d077c2c60736a
pradeeptomer4u <pradeeptomer4u@gmail.com>
2025-08-30 16:56:34 +0530

    changes

--- src/components/search/SearchContent.tsx
+++ src/components/search/SearchContent.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
-import searchService, { SearchResults } from '../../services/searchService';
+import { SearchResults } from '../../services/searchService';
 import { Product } from '../../types/product';
 import MetaTags from '../../components/SEO/MetaTags';
 

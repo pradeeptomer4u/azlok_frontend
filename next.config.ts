64d1509248884f98627230ccb87fd803e4967bc2
Pradeep Tomar <16862612+pradeeptomer4u@users.noreply.github.com>
2025-08-30 21:09:23 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
+      }, {
+        protocol: 'https',
+        hostname: 'pub-4f4e78fc0ec74271a702caabd7e4e13d.r2.dev',
+        port: '',
+        pathname: '/**',
       },
     ],
   },

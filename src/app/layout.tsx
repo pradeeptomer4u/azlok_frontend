496ada1467260b4f7e5824d2be0b67fb229dc2ed
pradeeptomer4u <pradeeptomer4u@gmail.com>
2025-09-10 06:54:24 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,8 +84,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/favicon.ico" type="image/x-icon">
-      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="google-site-verification" content="your-google-site-verification-code" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />

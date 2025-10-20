7dc2548f60b7a3f4a8fc9deb8dbcb4d6bf6448fb
pradeeptomer4u <pradeeptomer4u@gmail.com>
2025-10-20 10:31:46 +0530

    changes

--- src/app/account/invoices/[id]/page.tsx
+++ src/app/account/invoices/[id]/page.tsx
@@ -1,10 +1,12 @@
 import InvoiceDetailWrapper from './InvoiceDetailWrapper';
+import { Metadata } from 'next';
 
-type InvoiceParams = {
-  id: string;
+type PageProps = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export default function Page({ params }: { params: InvoiceParams }) {
+export default function Page({ params }: PageProps) {
   const { id } = params;
   return <InvoiceDetailWrapper id={id} />;
 }

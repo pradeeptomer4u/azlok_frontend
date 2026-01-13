7918b207b3eb8ecf148a2368e9bd1ce743acd328
pradeeptomer4u <pradeeptomer4u@gmail.com>
2026-01-13 11:21:55 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
